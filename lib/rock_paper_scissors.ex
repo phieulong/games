937b6d6379bbc7cd472faabb0fc0ba7dbf592d05
Nguyễn Quang Nhật <phieulong97@gmail.com>
2023-12-14 16:37:39 +0700

    finish Games: Rock Paper Scissors

---
+++ lib/rock_paper_scissors.ex
@@ -0,0 +1,29 @@
+defmodule Games.RockPaperScissors do
+  def play do
+    options = ["rock", "paper", "scissors"]
+    win_condition = [{"rock", "scissors"}, {"scissors", "paper"}, {"paper", "rock"}]
+    rand = Enum.random(options)
+    is_continue(rand, options, win_condition)
+  end
+
+  defp is_continue(rand, options, win_condition) do
+    messages = "(#{Enum.join(options, "\\")}): "
+
+    player_choice =
+      IO.gets(messages)
+      |> String.replace("\n", "")
+      |> String.trim()
+      |> String.downcase()
+
+    cond do
+      {player_choice, rand} in win_condition ->
+        IO.puts("You win! #{player_choice} beats #{rand}.!")
+
+      {rand, player_choice} in win_condition ->
+        IO.puts("You lose! #{rand} beats #{player_choice}.")
+
+      true ->
+        IO.puts("It's a tie!")
+    end
+  end
+end
